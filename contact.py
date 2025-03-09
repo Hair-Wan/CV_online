c05bf7afdfbe2ffd81e2e16aa804c04578205fb8
eblanc13 <erwan.blanchet@universite-paris-saclay.fr>
2025-03-10 00:08:42 +0100

    test gigitgnore

--- contact.py
+++ contact.py
@@ -12,5 +12,5 @@ with st.container(border=True):
 with st.expander("📍 Voir les coordonnées"):
     st.write("📌 **Adresse :** 123, Rue de la Tech, Paris, France")
     st.write("📞 **Téléphone :** +33 1 23 45 67 89")
-    st.write("✉️ **Email :** contact@exemple.com")
+    st.write("✉️ **Email :** contact@exemple.com") 
 st.divider()
